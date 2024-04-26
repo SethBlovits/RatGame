edf5f93fa43bada40dcb4d87f5c2881da84d13ad
SethBlovits <sethblovits@gmail.com>
2024-04-25 23:07:53 -0400

    added text chat functionality

--- RatBurglar/Assets/Scripts/chatUI.cs
+++ RatBurglar/Assets/Scripts/chatUI.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using Unity.Netcode;
-using System.Security.Principal;
 public class chatUI : NetworkBehaviour
 {
     // Start is called before the first frame update
@@ -17,8 +16,29 @@ public class chatUI : NetworkBehaviour
     [SerializeField]
     private GameObject message;
     private ulong[] directClientIDs = new ulong[2];
+    public TMP_InputField inputField;
+    public RectTransform rectTransform;
     private void Start(){
         enterButton.onClick.AddListener(EnterEventButton);
+        
+        inputField.DeactivateInputField();
+    }
+    public void Update(){
+        if(Input.GetKeyDown("t")){
+            inputField.ActivateInputField();
+            NetworkManager.LocalClient.PlayerObject.GetComponent<movement>().stuck = true;
+            rectTransform.localPosition = Vector3.zero;
+                
+            
+        }
+        if(Input.GetKeyDown(KeyCode.Return)){
+            SendMessageServerRpc(inputText.text);
+            inputField.DeactivateInputField();
+            NetworkManager.LocalClient.PlayerObject.GetComponent<movement>().stuck = false;
+            rectTransform.localPosition = new Vector3(0,2000,0);
+            
+        }
+
     }
     private void EnterEventButton(){
         //NewMessage("player2" , "hello message");
